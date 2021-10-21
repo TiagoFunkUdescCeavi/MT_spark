55b72f5de76b679f2355363466c191dbe3616a1e
Tiago Funk <tiagoff.tf@gmail.com>
2021-10-21 16:53:13 -0300

    Prova de conceito

--- external_program.py
+++ external_program.py
@@ -6,7 +6,7 @@ spark_conf = SparkConf().setAppName("YourTest").setMaster("local[*]")
 
 sc = SparkContext.getOrCreate(spark_conf)
 
-SIZE = 1000
+SIZE = 8
 
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
@@ -15,14 +15,16 @@ print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 def run_external_program( n ):
-	result = subprocess.run(['./app', str( n )], check=True, stdout=subprocess.PIPE, universal_newlines=True)
-	value = int( result.stdout )
+	#result = subprocess.run(['./app', str( n )], check=True, stdout=subprocess.PIPE, universal_newlines=True)
+	result = subprocess.run(['../TOP_GRASP_TS_PR/Project/TOP_GRASP_TS_PR', "1", "1", "1011742676", "../TOP_GRASP_TS_PR/instances/set_4_4/p4.4.l.txt", "--alpha", "0.98", "--iterations", "1000", "--path", "y", "--margin", "1.42", "--removeOperator", "w", "--removePercentage", "0.56", "--shuffleOperator", "e", "--addOperator", "b" ], check=True, stdout=subprocess.PIPE, universal_newlines=True)
+	value = float( result.stdout )
 	return value
 
 nums = list( range( 0, SIZE ) )
 
-nums_rdd = sc.parallelize(nums)
-a = nums_rdd.take( SIZE )
+nums_rdd = sc.parallelize(nums,4)
+
+print("Number of Partitions: "+str(nums_rdd.getNumPartitions()))
 
 pairs = nums_rdd.map(lambda x: (x, run_external_program( x ) ) )
 a = pairs.take( SIZE )
