812e5fb05da0ac39a8faf84e69183e95634a2c0d
Tiago Funk <tiagoff.tf@gmail.com>
2021-10-21 15:55:33 -0300

    Alteraçao para funcionar em versoes mais antigas do python

--- external_program.py
+++ external_program.py
@@ -1,14 +1,21 @@
 from pyspark import SparkContext, SparkConf
 import subprocess
+import sys
 
 spark_conf = SparkConf().setAppName("YourTest").setMaster("local[*]")
 
 sc = SparkContext.getOrCreate(spark_conf)
 
-SIZE = 100
+SIZE = 1000
+
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
+print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
 
 def run_external_program( n ):
-	result = subprocess.run(['./app', str( n )],  capture_output=True, text=True)
+	result = subprocess.run(['./app', str( n )], check=True, stdout=subprocess.PIPE, universal_newlines=True)
 	value = int( result.stdout )
 	return value
 
