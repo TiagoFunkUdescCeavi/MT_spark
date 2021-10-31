f1b298bff018d1520e08a75329647e55bae468aa
Tiago Funk <tiagoff.tf@gmail.com>
2021-10-31 10:20:22 -0300

    Escrevi testes para a classe Solução

--- project/Solution.py
+++ project/Solution.py
@@ -1,7 +1,8 @@
 from Utils import calculate_distance
+from Vertice import Vertice
 
 class Solution:    
-    def __init__( self, number_paths, time_per_path ) -> None:
+    def __init__( self, number_paths: int, time_per_path: float ):
         self.number_paths = number_paths
         self.paths = []
         self.path_rewards = []
@@ -14,53 +15,42 @@ class Solution:
         self.total_rewards = 0.0
         self.total_time = 0.0
         self.used_vertices = []
-    
-    def add_initial_and_final_vertices( self, path, initial_vertice, final_vertice ):
-        if self.check_if_path_is_valid( path ):
-            raise RuntimeError("Erro")
-        new_time = calculate_distance( initial_vertice, final_vertice )
-        if self.time_per_path < new_time:
-            raise RuntimeError("Erro")
-        self.paths[ path ].append( initial_vertice )
-        self.paths[ path ].append( final_vertice )
-        self.path_times[ path ] = new_time
-        self.total_time += new_time
 
-    def update_time_per_path( self, time_per_path ):
+    def __update_time_per_path( self, time_per_path: float ):
         self.time_per_path = time_per_path
 
-    def update_reward_in_add( self, path, vertice ):
+    def __update_reward_in_add( self, path: int, vertice: Vertice ):
         self.path_rewards[ path ] += vertice.get_reward()
         self.total_rewards += vertice.get_reward()
     
-    def update_reward_in_rewrite( self, path, position, v ):
+    def __update_reward_in_rewrite( self, path: int, position: int, v: Vertice ):
         vertice_to_remove          = self.paths[ path ][ position ]
         self.path_rewards[ path ] -= vertice_to_remove.get_reward()
         self.total_rewards        -= vertice_to_remove.get_reward()
         self.path_rewards[ path ] += v.get_reward()
         self.total_rewards        += v.get_reward()
 
-    def update_reward_in_remove( self, path, position ):
+    def __update_reward_in_remove( self, path: int, position: int ):
         vertice_to_remove = self.paths[ path ][ position ]
         self.path_rewards[ path ] -= vertice_to_remove.get_reward()
         self.total_rewards -= vertice_to_remove.get_reward()
     
-    def update_time( self, path, new_time ):
-        if self.check_if_path_is_valid( path ):
+    def __update_time( self, path: int, new_time: float ):
+        if self.__check_if_path_is_valid( path ):
             return
         self.total_time += new_time - self.path_times[ path ]
         self.path_times[ path ] = new_time
 
-    def add_in_path( self, path, position, vertice ):
+    def __add_in_path( self, path: int, position: int, vertice: Vertice ):
         self.paths[ path ].insert( position, vertice )
         self.used_vertices.append( vertice )
     
-    def remove_in_path( self, path, position ):
+    def __remove_in_path( self, path: int, position: int ):
         v = self.paths[ path ][ position ]
         self.used_vertices.remove( v )
         self.paths[ path ].remove( v )
 
-    def calculate_time_in_add( self, path, position, v ):
+    def __calculate_time_in_add( self, path: int, position: int, v: Vertice ):
         previous = self.paths[ path ][ position-1 ]
         next     = self.paths[ path ][ position ]
         old_distance             = calculate_distance( previous, next )
@@ -68,7 +58,7 @@ class Solution:
         new_distance_to_next     = calculate_distance( next, v )
         return self.path_times[ path ] - old_distance + new_distance_to_previous + new_distance_to_next
 
-    def calculate_time_in_rewrite( self, path, position, v ):
+    def __calculate_time_in_rewrite( self, path: int, position: int, v: Vertice ):
         previous = self.paths[ path ][ position-1 ]
         middle   = self.paths[ path ][ position ]
         next     = self.paths[ path ][ position+1 ]
@@ -78,7 +68,7 @@ class Solution:
         new_dist_to_next     = calculate_distance( v, next )
         return self.path_times[ path ] - old_dist_to_previous - old_dist_to_next + new_dist_to_previous + new_dist_to_next
     
-    def calculate_time_in_remove( self, path, position ):
+    def __calculate_time_in_remove( self, path: int, position: int ):
         previous = self.paths[ path ][ position-1 ]
         middle   = self.paths[ path ][ position ]
         next     = self.paths[ path ][ position+1 ]
@@ -87,83 +77,92 @@ class Solution:
         new_distance             = calculate_distance( previous, next )
         return self.path_times[ path ] - old_distance_to_previous - old_distance_to_next + new_distance
     
-    def recalculate_time( self, path ):
+    def __recalculate_time( self, path: int ):
         sum = 0.0
-        for i in range( len( self.paths[ path ] ) ):
+        for i in range( len( self.paths[ path ] ) - 1 ):
             sum += calculate_distance( self.paths[ path ][ i ], self.paths[ path ][ i+1 ] )
         return sum
     
-    def check_if_vertice_is_used( self, vertice ):
+    def __check_if_vertice_is_used( self, vertice: Vertice ):
         for v in self.used_vertices:
             if vertice.equals( v ):
                 return True
         return False
     
-    def check_if_path_is_valid( self, path ):
-        return path < 0 and path >= len( self.paths )
+    def __check_if_path_is_valid( self, path: int ):
+        if path < 0 or path >= len( self.paths ):
+            raise Exception("index of path is invalid")
     
-    def check_if_path_position_is_valid( self, path, position ):
-        return self.check_if_path_is_valid( path ) and ( position < 1 and position >= len( self.paths[ path ] ) )
+    def __check_if_path_and_position_is_valid( self, path: int, position: int ):
+        self.__check_if_path_is_valid( path )
+        if position < 1 or position >= len( self.paths[ path ] ):
+            raise Exception("index of position is invalid")
     
-    def check_if_position_is_initial_or_final( self, path, position ):
+    def __check_if_position_is_initial_or_final( self, path: int, position: int ):
         size = len( self.paths[ path ] ) -1
-        return position == 0 and position == size
-    
-    def add( self, path, v ):
-        if( self.check_if_path_is_valid( path ) ):
-            return False
-        position = len( self.paths[ path ] ) - 1
-        return self.add_in_position( path, position, v )
-    
-    def add_in_position( self, path, position, v ):
-        if self.check_if_path_position_is_valid( path, position ):
-            return False
-        if self.check_if_vertice_is_used( v ) :
-            return False
-        if v == None :
-            return False
-    
-        new_time = self.calculate_time_in_add( path, position, v )
+        return position == 0 or position == size
 
-        if( self.time_per_path > new_time ):
-            self.update_reward_in_add( path, v )
-            self.add_in_path( path, position, v )
-            self.update_time( path, new_time )
+    def add_initial_and_final_vertices( self, path: int, initial_vertice: Vertice, final_vertice: Vertice ):
+        self.__check_if_path_is_valid( path )
+        new_time = calculate_distance( initial_vertice, final_vertice )
+        if self.time_per_path < new_time:
+            raise Exception("Distance between initial and final vertice is bigger than time_per_paths")
+        self.paths[ path ].append( initial_vertice )
+        self.paths[ path ].append( final_vertice )
+        self.path_times[ path ] = new_time
+        self.total_time += new_time
+    
+    def add( self, path: int, vertice: Vertice ) -> bool:
+        self.__check_if_path_is_valid( path )
+        position = len( self.paths[ path ] ) - 1
+        return self.add_in_position( path, position, vertice )
+    
+    def add_in_position( self, path: int, position: int, vertice: Vertice ) -> bool:
+        self.__check_if_path_and_position_is_valid( path, position )
+        if self.__check_if_vertice_is_used( vertice ) :
+            return False
+        if vertice == None :
+            raise Exception("Vertice is None")
+    
+        new_time = self.__calculate_time_in_add( path, position, vertice )
+
+        if( self.time_per_path >= new_time ):
+            self.__update_reward_in_add( path, vertice )
+            self.__add_in_path( path, position, vertice )
+            self.__update_time( path, new_time )
             return True
     
         return False
 
     def rewrite( self, path, position, v ):
-        if self.check_if_path_position_is_valid( path, position ) :
+        if self.__check_if_path_and_position_is_valid( path, position ) :
             return False
-        if self.check_if_vertice_is_used( v ) :
+        if self.__check_if_vertice_is_used( v ) :
             return False
 
-        new_time = self.calculate_time_in_rewrite( path, position, v )
+        new_time = self.__calculate_time_in_rewrite( path, position, v )
 
         if self.time_per_path > new_time :
-            self.update_reward_in_rewrite( path, position, v )
-            self.remove_in_path( path, position )
-            self.add_in_path( path, position, v )
-            self.update_time( path, new_time )
+            self.__update_reward_in_rewrite( path, position, v )
+            self.__remove_in_path( path, position )
+            self.__add_in_path( path, position, v )
+            self.__update_time( path, new_time )
             return True
     
         return False
 
     def swap( self, path, pos1, pos2 ):
-        if self.check_if_path_position_is_valid( path, pos1 ) :
-            return False
-        if self.check_if_path_position_is_valid( path, pos2 ) :
-            return False
+        self.__check_if_path_and_position_is_valid( path, pos1 )
+        self.__check_if_path_and_position_is_valid( path, pos2 )
 
         v = self.paths[ path ][ pos1 ]
         self.paths[ path ][ pos1 ] = self.paths[ path ][ pos2 ]
         self.paths[ path ][ pos2 ] = v
 
-        new_time = self.recalculate_time( path )
+        new_time = self.__recalculate_time( path )
 
         if self.time_per_path > new_time :
-            self.update_time( path, new_time )
+            self.__update_time( path, new_time )
             return True
         else:
             v = self.paths[ path ][ pos1 ]
@@ -172,14 +171,12 @@ class Solution:
             return False
     
     def swap( self, path1, pos1, path2, pos2 ):
-        if( self.check_if_path_position_is_valid( path1, pos1 ) ):
-            return False
-        if( self.check_if_path_position_is_valid( path2, pos2 ) ):
-            return False
-        if( self.check_if_position_is_initial_or_final( path1, pos1 ) ):
-            return False
-        if( self.check_if_position_is_initial_or_final( path2, pos2 ) ):
-            return False
+        self.__check_if_path_and_position_is_valid( path1, pos1 )
+        self.__check_if_path_and_position_is_valid( path2, pos2 )
+        if( self.__check_if_position_is_initial_or_final( path1, pos1 ) ):
+            raise Exception("do not swap initial or final vertice")
+        if( self.__check_if_position_is_initial_or_final( path2, pos2 ) ):
+            raise Exception("do not swap initial or final vertice")
 
         reward_1 = self.paths[ path1 ][ pos1 ].get_reward()
         reward_2 = self.paths[ path2 ][ pos2 ].get_reward()
@@ -188,12 +185,12 @@ class Solution:
         self.paths[ path1 ][ pos1 ] = self.paths[ path2 ][ pos2 ]
         self.paths[ path2 ][ pos2 ] = v
 
-        new_time_path_1 = self.recalculate_time( path1 )
-        new_time_path_2 = self.recalculate_time( path2 )
+        new_time_path_1 = self.__recalculate_time( path1 )
+        new_time_path_2 = self.__recalculate_time( path2 )
 
         if( (self.time_per_path > new_time_path_1 and self.time_per_path > new_time_path_2) ):
-            self.update_time( path1, new_time_path_1 )
-            self.update_time( path2, new_time_path_2 )
+            self.__update_time( path1, new_time_path_1 )
+            self.__update_time( path2, new_time_path_2 )
             self.path_rewards[ path1 ] += reward_2 - reward_1
             self.path_rewards[ path2 ] += reward_1 - reward_2
             return True
@@ -204,54 +201,52 @@ class Solution:
             return False
 
     def remove( self, path, position ):
-        if( self.check_if_path_position_is_valid( path, position ) ):
+        if( self.__check_if_path_and_position_is_valid( path, position ) ):
             return False
-        self.update_reward_in_remove( path, position )
-        new_time = self.calculate_time_in_remove( path, position )
-        self.update_time( path, new_time )
-        self.remove_in_path( path, position )
+        self.__update_reward_in_remove( path, position )
+        new_time = self.__calculate_time_in_remove( path, position )
+        self.__update_time( path, new_time )
+        self.__remove_in_path( path, position )
         return True
     
-    def move( self, path1, position1, path2, position2 ):
-        if( self.check_if_path_position_is_valid( path1, position1 ) ):
-            return False
-        if( self.check_if_path_position_is_valid( path2, position2 ) ):
-            return False
+    def move( self, path1: int, position1: int, path2: int, position2: int ):
+        self.__check_if_path_and_position_is_valid( path1, position1 )
+        self.__check_if_path_and_position_is_valid( path2, position2 )
 
         v = self.paths[ path1 ][ position1 ]
 
-        new_time_path_1 = self.calculate_time_in_remove( path1, position1 )
-        new_time_path_2 = self.calculate_time_in_add( path2, position2, v )
+        new_time_path_1 = self.__calculate_time_in_remove( path1, position1 )
+        new_time_path_2 = self.__calculate_time_in_add( path2, position2, v )
 
         if( self.time_per_path > new_time_path_2 ):
-            self.update_time( path1, new_time_path_1 )
-            self.update_time( path2, new_time_path_2 )
-            self.update_reward_in_remove( path1, position1 )
-            self.remove_in_path( path1, position1 )
-            self.update_reward_in_add( path2, v )
-            self.add_in_path( path2, position2, v )
+            self.__update_time( path1, new_time_path_1 )
+            self.__update_time( path2, new_time_path_2 )
+            self.__update_reward_in_remove( path1, position1 )
+            self.__remove_in_path( path1, position1 )
+            self.__update_reward_in_add( path2, v )
+            self.__add_in_path( path2, position2, v )
             return True
         
         return False
     
     def is_empty( self, path ):
-        if( self.check_if_path_is_valid( path ) ):
-            return True
-        return len( self.paths[ path ] ) != 2
+        if( self.__check_if_path_is_valid( path ) ):
+            return False
+        return len( self.paths[ path ] ) == 2
     
     def get_last_path_vertice_in_path( self, path ):
-        if( self.check_if_path_is_valid( path ) ):
+        if( self.__check_if_path_is_valid( path ) ):
             return None
         last_position = len( self.paths[ path ] ) - 1 - 1
         return self.paths[ path ][ last_position ]
     
     def get_vertice_in_path( self, path, position ):
-        if( self.check_if_path_position_is_valid( path, position ) ):
+        if( self.__check_if_path_and_position_is_valid( path, position ) ):
             return None
         return self.paths[ path ][ position ]
     
     def get_rewards( self, path ):
-        if( not self.check_if_path_is_valid( path ) ):
+        if( not self.__check_if_path_is_valid( path ) ):
             return self.path_rewards[ path ]
     
     def get_total_rewards( self ):
@@ -261,7 +256,7 @@ class Solution:
         return self.total_time
     
     def get_distance( self, path, position ):
-        if( self.check_if_path_position_is_valid( path, position ) ):
+        if( self.__check_if_path_and_position_is_valid( path, position ) ):
             return -1.0
         if( len( self.paths[ path ] ) == 2 ):
             return -1.0
@@ -270,7 +265,7 @@ class Solution:
         return distance
     
     def get_time_path( self, path ):
-        if( self.check_if_path_is_valid( path ) ):
+        if( self.__check_if_path_is_valid( path ) ):
             return -1.0
         return self.path_times[ path ]
 
@@ -281,8 +276,7 @@ class Solution:
         return len( self.paths )
 
     def get_length_of_path( self, path ):
-        if self.check_if_path_is_valid( path ):
-            return -1
+        self.__check_if_path_is_valid( path )
         return len( self.paths[ path ] )
     
     def get_total_length_of_path( self ):
---
+++ project/test_solution.py
@@ -0,0 +1,242 @@
+from Solution import Solution
+from Vertice import Vertice
+import unittest
+
+class test_solution( unittest.TestCase ):
+    def test_init( self ):
+        sol = Solution( 2, 7.5 )
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 7.5 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 0.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 0.0 )
+        self.assertAlmostEqual( sol.get_rewards( 0 ), 0.0 )
+        self.assertAlmostEqual( sol.get_rewards( 1 ), 0.0 )
+        self.assertAlmostEqual( sol.get_time_path( 0 ), 0.0 )
+        self.assertAlmostEqual( sol.get_time_path( 1 ), 0.0 )
+    
+    def test_set_initial_and_final_vertices( self ):
+        initial = Vertice( 0.0, 0.0, 0 )
+        final = Vertice( 3.0, 4.0, 0 )
+        sol = Solution( 2, 7.5 )
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 7.5 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 0.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 10.0 )
+        for i in range( 2 ):
+            self.assertAlmostEqual( sol.get_length_of_path( i ), 2 )
+            self.assertAlmostEqual( sol.get_time_path( i ), 5.0 )
+            self.assertAlmostEqual( sol.get_rewards( i ), 0 )
+    
+    def test_set_invalid_initial_and_final_vertices( self ):
+        initial = Vertice( 0.0, 0.0, 0 )
+        final = Vertice( 3.0, 4.0, 0 )
+        sol = Solution( 2, 7.5 )
+
+        self.assertRaises( Exception, sol.add_initial_and_final_vertices, -1, initial, final)
+        self.assertRaises( Exception, sol.add_initial_and_final_vertices, 2, initial, final)
+        self.assertRaises( Exception, sol.add_initial_and_final_vertices, 59, initial, final)
+    
+    def test_set_initial_and_final_vertices_with_invalid_length( self ):
+        initial = Vertice( 0.0, -7.0, 0 )
+        final = Vertice( 0.0, 7.0, 0 )
+        sol = Solution( 2, 13.3 )
+
+        self.assertRaises( Exception, sol.add_initial_and_final_vertices, 0, initial, final)
+    
+    def test_add( self ):
+        initial = Vertice( 0.0, 0.0, 0 )
+        final = Vertice ( 3.0, 4.0, 0 )
+        new_vertice = Vertice( 6.0, 8.0, 5 )
+        sol = Solution( 2, 15.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        self.assertAlmostEqual( sol.add( 0, new_vertice ), True )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 0 ), 3 )
+        self.assertAlmostEqual( sol.get_time_path( 0 ), 15.0 )
+        self.assertAlmostEqual( sol.get_rewards( 0 ), 5 )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 1 ), 2 )
+        self.assertAlmostEqual( sol.get_time_path( 1 ), 5.0 )
+        self.assertAlmostEqual( sol.get_rewards( 1 ), 0 )
+
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 15.0 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 5.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 20.0 )
+
+    def test_invalid_adds( self ):
+        initial = Vertice( 0.0, 0.0, 0 )
+        final = Vertice ( 3.0, 4.0, 0 )
+        new_vertice = Vertice( 6.0, 8.0, 5 )
+        sol = Solution( 2, 7.5 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        self.assertRaises( Exception, sol.add, -1, new_vertice )
+        self.assertRaises( Exception, sol.add, 2, new_vertice )
+        self.assertRaises( Exception, sol.add, 1, None )
+        self.assertAlmostEqual( sol.add( 1, new_vertice ), False )
+    
+    def test_move( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 9.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertAlmostEqual( sol.move( 1,2,0,3 ), True )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 0 ), 5 )
+        self.assertAlmostEqual( sol.get_time_path( 0 ), 8.8344, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 0 ), 11 )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 1 ), 3 )
+        self.assertAlmostEqual( sol.get_time_path( 1 ), 3.9095, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 1 ), 4 )
+
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 9.0 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 15.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 12.7439, places=4 )
+
+    def test_invalid_move( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 7.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertAlmostEqual( sol.move( 1,2,0,3 ), False )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 0 ), 4 )
+        self.assertAlmostEqual( sol.get_time_path( 0 ), 6.4088, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 0 ), 3 )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 1 ), 4 )
+        self.assertAlmostEqual( sol.get_time_path( 1 ), 4.4366, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 1 ), 12 )
+
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 7.0 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 15.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 10.8453, places=4 )
+    
+    def test_invalid_position_on_move( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 7.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertRaises( Exception, sol.move, 1, 2, 0, 4 )
+        self.assertRaises( Exception, sol.move, 1, 0, 0, 3 )
+        self.assertRaises( Exception, sol.move, 1, -2, 0, 3 )
+    
+    def test_swap( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 7.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertAlmostEqual( sol.swap( 1, 2, 0, 1 ), True )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 0 ), 4 )
+        self.assertAlmostEqual( sol.get_time_path( 0 ), 4.2708, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 0 ), 10 )
+
+        self.assertAlmostEqual( sol.get_length_of_path( 1 ), 4 )
+        self.assertAlmostEqual( sol.get_time_path( 1 ), 6.3163, places=4 )
+        self.assertAlmostEqual( sol.get_rewards( 1 ), 5 )
+
+        self.assertAlmostEqual( sol.get_number_paths(), 2 )
+        self.assertAlmostEqual( sol.get_time_per_path(), 7.0 )
+        self.assertAlmostEqual( sol.get_total_rewards(), 15.0 )
+        self.assertAlmostEqual( sol.get_total_time(), 10.5871, places=4 )
+
+    def test_invalid_move( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 6.3 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertAlmostEqual( sol.swap( 1, 2, 0, 1 ), False )
+    
+    def test_invalid_position_on_swap( self ):
+        initial = Vertice( 1.0, 1.0, 0 )
+        final = Vertice( 2.0, 2.0, 0 )
+        v1 = Vertice( 2.3, 4.5, 1 )
+        v2 = Vertice( 1.8, 2.1, 2 )
+        v3 = Vertice( 2.2, 3.3, 4 )
+        v4 = Vertice( 2.7, 3.1, 8 )
+        sol = Solution( 2, 7.0 )
+
+        for i in range( 2 ):
+            sol.add_initial_and_final_vertices( i, initial, final )
+        
+        sol.add( 0, v1 )
+        sol.add( 0, v2 )
+        sol.add( 1, v3 )
+        sol.add( 1, v4 )
+
+        self.assertRaises( Exception, sol.swap, 1, 2, 0, 3 )
+        self.assertRaises( Exception, sol.move, 1, 0, 0, 3 )
+        self.assertRaises( Exception, sol.move, 1, -2, 0, 3 )
